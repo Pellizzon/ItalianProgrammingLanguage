1e070a8c8b92b8b746e0a2e95499f3c5d8f4a820
Pellizzon <matheuspellizzon@hotmail.com>
2021-05-16 17:12:04 -0300

    remove requirement to be a .c file

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from components.node import symbolTable
 
 if __name__ == "__main__":
 
-    if not sys.argv[1].endswith(".c"):
-        raise ValueError("Input file must have '.c' extension")
-
     with open(f"{sys.argv[1]}", "r") as f:
         inputData = f.read()
 
